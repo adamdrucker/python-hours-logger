7a1309c751d8ab209aef86f21c1e793ddb01912d
Adam <36452622+adamdrucker@users.noreply.github.com>
2020-03-12 17:17:31 -0400

    Add files via upload

--- scratch.py
+++ scratch.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+import re
 
 # x = datetime.datetime.now()
 # print(x)
@@ -24,31 +24,66 @@ while not date_check(sDate):
     break
 '''
 
-sTime = input("Enter a time: ")
-dt = datetime.strptime(sTime, "%H:%M")
-hour = dt.strftime("%H")
-minute = dt.strftime("%M")
+# time_format = "%H:%M"
+# sTime = input("Enter a time: ")
+#
+# def time_check(i):
+#     # This checks for the correct format as described below, returns bool
+#         try:
+#             dt = datetime.strptime(i, "%H:%M")
+#             return True
+#         except ValueError:
+#             return False
+#
+# def new_time_check(i):
+#     checkt = time_check(i)
+#     while not checkt:
+#         # Checks returned bool and loops for correct input again
+#         print("Wrong format entered, try again.")
+#         i = input("Enter a time: ")
+#         checkt = time_check(i)
+#
+#     dt = datetime.strptime(i, "%H:%M")
+#     d = dt.strftime("%H:%M")
+#
+#     while i != d:
+#         print("Time's do not match")
+#         i = input("Enter a time: ")
+#
+#
+# new_time_check(sTime)
+
+# while sTime != d:
+#     sTime = input("Enter a time: ")
+#     try:
+#         print("Wrong format, try again.")
+#         dt = datetime.strptime(sTime, "%H:%M")
+#         d = dt.strftime("%H:%M")
+#     except ValueError:
+#         print("value error")
+#         continue
+
 
 # This needs to break apart the input time, first passing the "XX:XX" format check,
 #  then make sure that each integer is either zero-padded if single digit or a double
 #   digit integer (along with checking that it is a valid time i.e. <24:00
 
-def time_check(i):
-    try:
-        datetime.strptime(i, "%H:%M")
-        return True
-    except ValueError:
-        return False
-
-
-while not time_check(sTime):
-    print("Incorrect format, try again.")
-    sTime = input("Enter a time: ")
-    # ---
-    time_check(sTime)
-    continue
-
-
-time = f"{hour}:{minute}"
-print(time)
+# def time_check(i):
+#     try:
+#         datetime.strptime(i, "%H:%M")
+#         return True
+#     except ValueError:
+#         return False
+#
+#
+# while not time_check(sTime):
+#     print("Incorrect format, try again.")
+#     sTime = input("Enter a time: ")
+#     # ---
+#     time_check(sTime)
+#     continue
+#
+#
+# time = f"{hour}:{minute}"
+# print(time)
 
